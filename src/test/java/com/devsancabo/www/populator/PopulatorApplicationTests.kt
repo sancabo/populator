6d3fc9ee20d85f335e3c86fd79357d2a01b3aa34
Santiago Cabo <newsancabo@gmail.com>
2023-01-25 23:43:30 -0300

    Replaced java file with kotlin file

---
+++ src/test/java/com/devsancabo/www/populator/PopulatorApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.devsancabo.www.populator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@SpringBootApplication
+class PopulatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
